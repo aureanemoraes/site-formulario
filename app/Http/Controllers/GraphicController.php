934ebf7cedcc36386b81baac911660a3ca6d7376
aureane moraes <aureane.moraes@outlook.com>
2019-03-18 21:57:38 -0300

    controller gráfico pronto

--- app/Http/Controllers/GraphicController.php
+++ app/Http/Controllers/GraphicController.php
@@ -13,26 +13,26 @@ class GraphicController extends Controller
 {
     public function show($id)
     {
-        $question = Question::where('form_id', '=', $id);
+        $question = Question::where('form_id', '=', $id)->get();
         $opts = Option::all();
+        $i=0;
 
-        $lava = new Lavacharts;
 
-        $options = \Lava::DataTable();
-
-        $options->addStringColumn('Opções')
-                ->addNumberColumn('Porcentagem');
         foreach($question as $q) {
-            foreach($opts as $o) {
-                if($o->question_id == $question->id) {
-                    $options->addRow([$o->name, $o->amount]);
+            $options[$i] = \Lava::DataTable();
+            $options[$i]->addStringColumn('Opções')
+                    ->addNumberColumn('Porcentagem');
+            foreach($opts as $o ) {
+                if($o->question_id == $q->id) {
+                    $options[$i]->addRow([$o->name, $o->amount]);
                 }
             }
-            $piechart = \Lava::PieChart($q->name, $options, [
-                'title' => 'Questões respondidas',
-                'is3D' => true
+                $piechart = \Lava::PieChart($q->name, $options[$i], [
+                    'title' => $q->name,
+                    'is3D' => true
             ]);
+            $i++;
         }
-        return view('graphics.show', compact('question', 'piechart'));
+        return view('graphics.show', compact('question'));
     }
 }
--- resources/views/graphics/show.blade.php
+++ resources/views/graphics/show.blade.php
@@ -1,9 +1,11 @@
 
-
-@foreach ( $question as $q)
-<div id="chart-div"></div>
-<?= Lava::render('PieChart', $q->name, 'chart-div') ?>
-
+@php ($i = 0)
+@php ($name = "chart-div")
+@foreach($question as $q)
+<div id="{{  $name. $i}}"></div>
+@php ($fullname = $name . $i)
+<?= Lava::render('PieChart', $q->name, $fullname) ?>
+@php ($i++ )
 @endforeach
 <h1>$piechart</h1>
-                        {{ var_dump($piechart) }}
+
